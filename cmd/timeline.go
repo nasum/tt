4658d3f8e0999a9ffd6297e21588e487749cb0f3
nasum <masaya.nasu@crowdworks.co.jp>
2017-11-19 21:32:42 +0900

    edit timeline func

--- cmd/timeline.go
+++ cmd/timeline.go
@@ -2,29 +2,43 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/dghubble/go-twitter/twitter"
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
 func timelineCmd(client twitter.Client) *cobra.Command {
+	htp := &twitter.HomeTimelineParams{}
 
 	cmd := &cobra.Command{
 		Use:   "timeline",
 		Short: "get your timeline",
 		Run: func(cmd *cobra.Command, args []string) {
-			tweets, res, err := client.Timelines.HomeTimeline(&twitter.HomeTimelineParams{
-				Count: 20,
-			})
+			tweets, res, err := client.Timelines.HomeTimeline(htp)
 
 			if err != nil {
 				fmt.Println(res)
 			}
 
 			for _, v := range tweets {
-				fmt.Print(v)
+				showTweet(v)
 			}
 		},
 	}
+
+	flags := cmd.Flags()
+	flags.IntVarP(&htp.Count, "count", "c", 20, "Set tweet count")
+	flags.Int64VarP(&htp.SinceID, "since-id", "s", 0, "Set since tweet id")
+	flags.Int64VarP(&htp.MaxID, "max-id", "m", 0, "Set max tweet id")
+
 	return cmd
+}
+
+func showTweet(tweet twitter.Tweet) {
+	green := color.New(color.FgGreen).SprintFunc()
+	yellow := color.New(color.FgYellow).SprintfFunc()
+	cyan := color.New(color.FgCyan).SprintfFunc()
+	fmt.Printf("%s %s %s %s \n", green(tweet.CreatedAt), yellow(strconv.FormatInt(tweet.ID, 10)), cyan("@"+tweet.User.ScreenName), tweet.Text)
 }
