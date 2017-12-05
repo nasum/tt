904bd2c23638d9199542406b519d58096afd671c
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-12-06 01:36:22 +0900

    show times in local timezone
    also return errors

--- lib/display_console.go
+++ lib/display_console.go
@@ -8,9 +8,14 @@ import (
 	"github.com/fatih/color"
 )
 
-func ShowTweet(tweet twitter.Tweet) {
+func ShowTweet(tweet twitter.Tweet) error {
 	green := color.New(color.FgGreen).SprintFunc()
 	yellow := color.New(color.FgYellow).SprintfFunc()
 	cyan := color.New(color.FgCyan).SprintfFunc()
-	fmt.Fprintf(color.Output, "%s\t%s\t%s\t%s\n", green(tweet.CreatedAt), yellow(strconv.FormatInt(tweet.ID, 10)), cyan("@"+tweet.User.ScreenName), tweet.Text)
+	createdAt, err := tweet.CreatedAtTime()
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(color.Output, "%s\t%s\t%s\t%s\n", green(createdAt.Local().Format("2006/01/02 15:04:05")), yellow(strconv.FormatInt(tweet.ID, 10)), cyan("@"+tweet.User.ScreenName), tweet.Text)
+	return nil
 }
